96e606d8366652279aeefba9a1d503919054ce7e
mg8033667@gmail.com <Mohamed Gamal>
2024-05-30 14:50:37 -0700

    encrypt_password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -14,3 +14,15 @@ def hash_password(password: str) -> bytes:
     encoded = password.encode()
     hashed = bcrypt.hashpw(encoded, bcrypt.gensalt())
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    '''
+    function to check if the password
+    matches the hased password.
+    '''
+    my_password = password.encode()
+    if bcrypt.checkpw(my_password, hashed_password):
+        return True
+    else:
+        return False
