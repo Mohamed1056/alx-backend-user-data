195ba158ff650ed7a62830e89d210ae8de471924
mg8033667@gmail.com <Mohamed Gamal>
2024-06-17 14:40:08 -0700

    adding user class that makes the database

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,32 +1,31 @@
-#!/user/bin/env python3
-"""
-creating a DOM for database called user.
+#!/usr/bin/env python3
+"""Module for Users.
 """
 
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-
 Base = declarative_base()
 
 
 class User(Base):
-    """
-    User class for connecting to sql
-    Its attributes:
-    __tablename__ : the name of the table in the
-    database.
-    id(int): store the id.
-    email(str): stores the email.
-    hashed_password(str): stores the hashed
-    password of the user.
-    session_id(str): stores the session id.
-    reset_token(str): stores the tokens.
+    """A class representing a user in the system.
+
+    Attributes:
+        __tablename__ (str): The name of the table in the database where
+        user records are stored.
+        id (int): The unique identifier of the user.
+        email (str): The email address of the user.
+        hashed_password (str): The hashed password of the user.
+        session_id (str): The session ID of the user, used to maintain
+        user sessions.
+        reset_token (str): The reset token of the user, used for password
+        resets.
     """
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250), nullable=False)
-    reset_token = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
